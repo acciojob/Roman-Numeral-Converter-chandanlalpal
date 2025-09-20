a9a39031f476bf7a210b9f30eafcff4505313056
chandan lal <148762955+chandanlalpal@users.noreply.github.com>
2025-09-20 08:05:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,17 @@
-const symbol = [
-    ['M', 1000],
-    ['CM', 900],
-    ['D', 500],
-    ['CD', 400],
-    ['C', 100],
-    ['XC', 90],
-    ['L', 50],
-    ['XL', 40],
-    ['X', 10],
-    ['IX', 9],
-    ['V', 5],
-    ['IV', 4],
-    ['I', 1]
-];
-
 let num = 14;  // Example 1 → XIV
 // let num = 798; // Example 2 → DCCXCVIII
-let result = "";
 
-symbol.forEach(([key, val]) => {
-    while (num >= val) {
-        num -= val;
-        result += key;
-    }
-});
+if (num <= 0 || num > 100000) {
+    console.log(""); // Handle error or out of range
+} else {
+    let result = "";
 
-console.log(result);
+    symbol.forEach(([key, val]) => {
+        while (num >= val) {
+            num -= val;
+            result += key;
+        }
+    });
+
+    console.log(result);
+}
