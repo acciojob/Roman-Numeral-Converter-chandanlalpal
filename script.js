022fbbe92a58391661fa8bc1356cc96b03229976
chandan lal <148762955+chandanlalpal@users.noreply.github.com>
2025-09-20 08:04:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,28 @@
-function convertToRoman(num) {
-  if (num <= 0 || num > 10000) {
-    return ""; // handle error or out of range
-  }
+const symbol = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+];
 
-  // Merge normal + special cases in descending order
-  const symbols = [
-    ["M", 1000],
-    ["CM", 900],
-    ["D", 500],
-    ["CD", 400],
-    ["C", 100],
-    ["XC", 90],
-    ["L", 50],
-    ["XL", 40],
-    ["X", 10],
-    ["IX", 9],
-    ["V", 5],
-    ["IV", 4],
-    ["I", 1]
-  ];
+let num = 14;  // Example 1 → XIV
+// let num = 798; // Example 2 → DCCXCVIII
+let result = "";
 
-  let result = "";
-
-  for (let [symbol, value] of symbols) {
-    while (num >= value) {
-      num -= value;
-      result += symbol;
-      
+symbol.forEach(([key, val]) => {
+    while (num >= val) {
+        num -= val;
+        result += key;
     }
-  }
+});
 
-  return result;
-}
-
-console.log(convertToRoman(14));       // Should return an error or empty string
-     //
+console.log(result);
