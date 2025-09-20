fd57f6d49a3d73cf9b6b078d2fc8fe81f84b7459
chandan lal <148762955+chandanlalpal@users.noreply.github.com>
2025-09-20 08:08:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-let num = 14;  // Example 1 → XIV
-// let num = 798; // Example 2 → DCCXCVIII
+// converter.js
+function convertToRoman(num) {
+  const symbols = [
+    ["M", 1000],
+    ["CM", 900],
+    ["D", 500],
+    ["CD", 400],
+    ["C", 100],
+    ["XC", 90],
+    ["L", 50],
+    ["XL", 40],
+    ["X", 10],
+    ["IX", 9],
+    ["V", 5],
+    ["IV", 4],
+    ["I", 1]
+  ];
 
-if (num <= 0 || num > 100000) {
-    console.log(""); // Handle error or out of range
-} else {
-    let result = "";
-
-    symbol.forEach(([key, val]) => {
-        while (num >= val) {
-            num -= val;
-            result += key;
-        }
-    });
-
-    console.log(result);
+  let result = "";
+  for (let [symbol, value] of symbols) {
+    while (num >= value) {
+      num -= value;
+      result += symbol;
+    }
+  }
+  return result;
 }
+
+module.exports = convertToRoman; // 👈 Export it
